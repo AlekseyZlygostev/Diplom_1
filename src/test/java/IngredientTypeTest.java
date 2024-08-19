eb39ff43586bc3a5bccf20b0373f789b5286d4cc
Aleksey Zlygostev <evil.aleks@yandex.ru>
2024-08-19 19:36:21 +0300

    Third commit

--- src/test/java/IngredientTypeTest.java
+++ src/test/java/IngredientTypeTest.java
@@ -1,2 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+import praktikum.IngredientType;
+
 public class IngredientTypeTest {
+    @Test
+    public void testSauce(){
+        Assert.assertNotNull(IngredientType.valueOf("SAUCE"));
+    }
+
+    @Test
+    public void testFilling(){
+        Assert.assertNotNull(IngredientType.valueOf("FILLING"));
+    }
 }
+
